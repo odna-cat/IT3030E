f4e183d031d8b2625ef6a7c31b3d45e0daeec7e8
odna-cat <shyshyshy795@protonmail.com>
2024-11-29 10:46:19 +0700

    Add complete exercise 5 solution

--- 0241127/Ex5.asm
+++ 0241127/Ex5.asm
@@ -9,30 +9,55 @@
 str1:	.asciz "enter a: "
 str2:	.asciz "enter b: "
 str3:	.asciz "a / b = "
+end:	.asciz "Both a and b are 0!"
 .text
 main:
+	li t0, 0		# reset sum
+	li a1, 0		# reset a and b
+	li a2, 0		# for next loop
+	
 	li a7, 4
 	la a0, str1
 	ecall
 	li a7, 5
-	ecall
-	mv t0, a0	# int sum = 0;
+	ecall			# t0 = sum;
+	add a1, a1, a0		# int a;
+	add t0, t0, a1		# int sum = a;
 	
 	li a7, 4
 	la a0, str2
 	ecall
 	li a7, 5
 	ecall
-	add t0, t0, a0	# sum = a + b;
+	add a2, a2, a0
+	add t0, t0, a2		# sum += b;
+	j check
+check:
+	bnez a1, avg		# if (a!=0 && b!=0) goto avg
+	bnez a2, avg		# if line 31 fails, then a = 0
+	j avg_end		# if line 32 fails, then a = b = 0 -> terminate program
+avg_end:
+	li a7, 4
+	la a0, end
+	ecall
+	li a7, 10
+	ecall
+avg:
 	li t1, 2
 	li t2, 0
-	fcvt.s.w f0, t0, dyn	# f0 = sum;
-	fcvt.s.w f1, t1, dyn	# f1 = 2;
-	fcvt.s.w f3, t2, dyn	# f3 = 0;
 	
-	fdiv.s f2, f0, f1, dyn	# f2 = avg
-	li a7, 2
-	fadd.s fa0, f3, f2, dyn
+	fcvt.s.w f0, t0, dyn	# float(sum);
+	fcvt.s.w f1, t1, dyn	# float(2);
+	fcvt.s.w f2, t2, dyn	# float(0); for some godforsaken reason
+	
+	fdiv.s f3, f0, f1, dyn
+	li a7, 4
+	la a0, str3
 	ecall
-	
-	
+	li a7, 2
+	fadd.s fa0, f2, f3, dyn
+	ecall
+	li a7, 11
+	li a0, 10
+	ecall
+	j main
